2bfc305fd54abf1fd2f9b11bf4b08815ee8170e8
Salvatore Cavallaro <plasticgo@gmail.com>
2018-03-09 14:52:09 +0100

    Ancora Uno

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!!!!
+                    You are logged in!!!! WHOAAA!
 
                     <example-component></example-component>
                 </div>
